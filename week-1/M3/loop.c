cedca6cb5ee89bff4c34a54dc814d6149d504292
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2024-04-07 16:26:06 +0600

    .

--- week-1/M3/loop.c
+++ week-1/M3/loop.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main(){
-printf("fdff");
+int main()
+{
+    int i = 0;
+    while (i = 0) {
+        printf("Hello\n");
+        i++;
+    }
     return 0;
 }
---
+++ week-1/M3/valuePrint.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 100; 1 <= i; i = i - 10)
+    {
+        printf("%d\n", i);
+    }
+    return 0;
+}
