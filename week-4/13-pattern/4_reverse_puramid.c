3600617c6d72d382aa504bc2b1c3bc510d72b1e8
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2024-04-28 14:08:31 +0600

    .

---
+++ week-4/13-pattern/4_reverse_puramid.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, s, k;
+    scanf("%d", &n);
+    s = 0;
+    k = n * 2 - 1;
+
+    for (int i = n; i >= 1; i++)
+    {
+        for(int j=1; )
+    }
+    return 0;
+}
